c85c33b4ca553755695d10e36c34fa7fb78d1587
Dylan Lecigne <dylanlecigne@MacBook-Air-de-Dylan.local>
2017-01-29 22:15:02 +0100

    Ajout de 2 controllers non terminés à peine commencé :)

---
+++ app/Controllers/Contact.php
@@ -0,0 +1,31 @@
+<?php
+
+/** 
+* Controller de contact 
+* @author Dylan Lecigne & Quentin Helleboid
+*/
+namespace App\Controllers;
+
+use Core\View;
+use Core\Controller;
+use Helpers\Url;
+
+class Contact extends Controller {
+
+	/* Appel de la Construction du parent (Controller) */
+    public function __construct() {
+        parent::__construct();
+    }
+
+
+    public function index(){
+    	/* A voir avec les templates*/
+    }
+
+    public function sendForm(){
+
+    }
+}
+
+
+?>
---
+++ app/Controllers/Gallery.php
@@ -0,0 +1,16 @@
+<?php
+
+/** 
+* Controller de contact 
+* @author Dylan Lecigne & Quentin Helleboid
+*/
+namespace App\Controllers;
+
+
+
+class Gallery extends Controllers{
+	
+}
+
+
+?>
