ac00bda7ba0c7f7fcb4ff21dd669c187ce34ebcc
Quentin Helleboid <Quentin@MacBook-Air-de-Quentin.local>
2017-01-31 23:07:56 +0100

    Ajout de la map

--- app/Views/About/about.php
+++ app/Views/About/about.php
@@ -1,10 +1,15 @@
 <div class="container">
-	<div class="row">
-	<p>
-	COUCOU C'est nous les tuches 
-	NTM 
+    <div class="row">
+        <p>
+            COUCOU C'est nous les tuches
+            NTM
 
-	LOREM IPEJDSQSGKHDC
-	</p>
-	</div>
+            LOREM IPEJDSQSGKHDC
+        </p>
+    </div>
+
+
+    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d630.5492306767339!2d2.176735!3d50.790459!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x7d1aa797d32ac591!2sGite+de+Langlebert!5e0!3m2!1sfr!2sus!4v1485900419535" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+
+</iframe>
 </div>
