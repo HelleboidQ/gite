97cce66978fac7f8bf8179b900b2bac13df1fe82
Dylan Lecigne <dylanlecigne@MacBook-Air-de-Dylan.local>
2017-02-28 14:02:58 +0100

    Ajout de la premiere version du footer

--- app/Templates/Business/footer.php
+++ app/Templates/Business/footer.php
@@ -10,6 +10,14 @@ Assets::js([
 echo $js; //place to pass data / plugable hook zone
 echo $footer; //place to pass data / plugable hook zone
 ?>
-
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <p>Copyright &copy; Powered by MDQ - 2017</p>
+                </div>
+            </div>
+        </div>
+    </footer>
 </body>
 </html>
