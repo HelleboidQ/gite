c322acfea5dbb7331438c754df892c919979924d
Dylan Lecigne <dylanlecigne@MacBook-Air-de-Dylan.local>
2017-05-15 22:43:20 +0200

    Mise en forme de la page contact

--- app/Views/Contact/contact.php
+++ app/Views/Contact/contact.php
@@ -1,28 +1,29 @@
 <div class="container">
 	<div class="row">
-        <a href="https://goo.gl/UWZ5b5"> Note Google ici </a>
-		<form class="form-horizontal" role="form" action="contact/envoieForm" method="post">
+        <a href="https://goo.gl/UWZ5b5" id="lien_notation" class="col-xs-12"> Dites nous tout! Notez notre g&icirc;te ici </a>
+        <h2 id="titre_contact" class="col-xs-12">Pour toutes questions suppl&eacute;mentaires, contactez nous: </h2>
+		<form class="form-horizontal" role="form" action="contact/envoieForm" method="post" id="form_email" >
                 <div class="form-group">
                     <label for="inputEmail1" class="col-lg-4 control-label"></label>
-                    <div class="col-lg-10">
+                    <div class="col-lg-12">
                         <input type="email" class="form-control" id="inputEmail1" name="mail" required placeholder="Email">
                     </div>
                 </div>
                 <div class="form-group">
                     <label for="text1" class="col-lg-4 control-label"></label>
-                    <div class="col-lg-10">
+                    <div class="col-lg-12">
                         <input type="text" class="form-control" id="text1" name="nom" required placeholder="Nom">
                     </div>
                 </div>
                 <div class="form-group">
                     <label for="text1" class="col-lg-4 control-label"></label>
-                    <div class="col-lg-10">
-                        <textarea  class="form-control" name="message" required placeholder="Message ..."></textarea>
+                    <div class="col-lg-12">
+                        <textarea  class="form-control" name="message" required placeholder="Message ..." rows="8" ></textarea>
                     </div>
                 </div>
                 <div class="form-group">
-                    <div class="col-lg-10">
-                        <button type="submit" name="envoyer" class="btn btn-success">Envoyer</button>
+                    <div class="col-lg-12">
+                        <button type="submit" name="envoyer" id="send_mail" class="btn btn-success">Envoyer</button>
                     </div>
                 </div>
             </form><!-- form -->
