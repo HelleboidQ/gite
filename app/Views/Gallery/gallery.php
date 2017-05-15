a07c8f63ea35d6fc5e27f88df36338bb7f1a14ea
Quentin Helleboid <Quentin@MacBook-Air-de-Quentin.local>
2017-05-15 22:46:15 +0200

    Modif image + nom

--- app/Views/Gallery/gallery.php
+++ app/Views/Gallery/gallery.php
@@ -1,12 +1,85 @@
 <div class="container">
 	<div class="row">
 
-		  {% for image in images %}
-		 	<div class="col-xs-4">
-			 	<div class="div_img row">
-			 		<img src="{{ image }}" class="col-xs-12 img-responsive" />
-			 	</div>
-		 	</div>
-		  {% endfor %}
+		<div class="col-xs-12">
+			<div class="row">
+				<div class="col-xs-4"> 
+				 	<img src="app/Templates/Business/Assets/images/cathedraleNotreDamePhilippeHudelle.jpg" class="col-xs-12 img-responsive" alt="Cathedrale" />
+				 	<p>Cathedrale Notre Dame Philippe Hudelle Balades en Audomarois</p> 
+				</div>
+
+				<div class="col-xs-4"> 
+				 	<img src="app/Templates/Business/Assets/images/CHAPELLEDESJESUITESOTRSO.jpg" class="col-xs-12 img-responsive" alt="" />
+				 	<p>2014 Facade renovee CHAPELLE DES JESUITES OTRSO</p> 
+				</div>
+
+				<div class="col-xs-4"> 
+				 	<img src="app/Templates/Business/Assets/images/BACOVE_2013OTRSO.jpg" class="col-xs-12 img-responsive" alt="" />
+				 	<p>BACOVE_2013OTRSO</p> 
+				</div>
+			</div>
+		</div>
+		<div class="col-xs-12">
+			<div class="row">
+		<div class="col-xs-4"> 
+		 	<img src="app/Templates/Business/Assets/images/MotteCastrale023.jpg" class="col-xs-12 img-responsive" alt="" />
+		 	<p>Balades en Audomarois, P.Hudelle - Motte Castrale 023</p> 
+		</div>
+
+		<div class="col-xs-4"> 
+		 	<img src="app/Templates/Business/Assets/images/claimaraisbareaux42010.jpg" class="col-xs-12 img-responsive" alt="" />
+		 	<p>claimarais bareaux4  2010-Photo Carl-Office de Tourisme de la Region de Saint-Omer</p> 
+		</div>
+
+		<div class="col-xs-4"> 
+		 	<img src="app/Templates/Business/Assets/images/claimaraispaysageprintemps.jpg" class="col-xs-12 img-responsive" alt="" />
+		 	<p>claimarais paysage printemps 063 2013-Photo Carl-Office de Tourisme de la Region de Saint-Omer</p> 
+		</div>
+</div>
+		</div>
+		<div class="col-xs-12">
+			<div class="row">
+		<div class="col-xs-4"> 
+		 	<img src="app/Templates/Business/Assets/images/claimarais_romelaere.jpg" class="col-xs-12 img-responsive" alt="" />
+		 	<p>claimarais romelaere promenade 04  2011-Photo Carl-Office de Tourisme de la Region de Saint-Omer</p> 
+		</div>
+
+		<div class="col-xs-4"> 
+		 	<img src="app/Templates/Business/Assets/images/ClairmaraisMaraisRomelaere.jpg" class="col-xs-12 img-responsive" alt="" />
+		 	<p>Clairmarais Marais Romelaere 03  2010 - Comimatex - Office de Tourisme de la Region de Saint-Omer</p> 
+		</div>
+
+		<div class="col-xs-4"> 
+		 	<img src="app/Templates/Business/Assets/images/DOMELaCoupole.JPG" class="col-xs-12 img-responsive" alt="" />
+		 	<p>DOME  La Coupole</p> 
+		</div>
+</div>
+		</div>
+		<div class="col-xs-12">
+			<div class="row">
+		<div class="col-xs-4"> 
+		 	<img src="app/Templates/Business/Assets/images/LACOUPOLEINTERIEURLaCoupole.JPG" class="col-xs-12 img-responsive" alt="" />
+		 	<p>LACOUPOLE INTERIEUR La Coupole</p> 
+		</div>
+
+		<div class="col-xs-4"> 
+		 	<img src="app/Templates/Business/Assets/images/RUINESSAINTBERTINOTRSO.JPG" class="col-xs-12 img-responsive" alt="" />
+		 	<p>RUINES SAINT BERTINOTRSO</p> 
+		</div>
+
+		<div class="col-xs-4"> 
+		 	<img src="app/Templates/Business/Assets/images/SaintOmerCathedraleNotre-Dame.jpg" class="col-xs-12 img-responsive" alt="" />
+		 	<p>Saint-Omer Cathedrale Notre-Dame 024 2013-PhotoCarl-Office de Tourisme de la Region de Saint-Omer</p> 
+		</div>
+</div>
+		</div>
+		<div class="col-xs-12">
+			<div class="row">
+		<div class="col-xs-4"> 
+		 	<img src="app/Templates/Business/Assets/images/vueaeriennemaraisaudomaroisIsnor.jpg" class="col-xs-12 img-responsive" alt="" />
+		 	<p>vue aerienne marais audomarois  Isnor.jpg</p> 
+		</div>
 	</div>
+</div>
+</div>
 </div>
