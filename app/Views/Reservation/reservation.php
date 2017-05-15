d17b89811a48e2dd6b70f7a0b6930432fd9d1fe9
Dylan Lecigne <dylanlecigne@MacBook-Air-de-Dylan.local>
2017-05-15 15:04:34 +0200

    Ajout du tableau non finis pour les tarifs

--- app/Views/Reservation/reservation.php
+++ app/Views/Reservation/reservation.php
@@ -1,1 +1,46 @@
 <iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=2&amp;hl=fr&amp;bgcolor=%23ffffff&amp;src=4s6e6aeqrns4jki0sn2cq7kc0o%40group.calendar.google.com&amp;color=%235229A3&amp;ctz=Europe%2FParis" style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+
+<!-- Tableau pour la semaine -->
+<h2>Tarification pour la semaine</h2>
+<table class="row">
+
+   <tr>
+       <th class="col-xs-8">Saison</th>
+       <th class="col-xs-4">Tarif</th>
+   </tr>
+   <tr>
+   		<td class="col-xs-8">Haute Saison</td>
+   		<td class="col-xs-4">400&euro;</td>
+   </tr>
+   <tr>
+   		<td class="col-xs-8">Autres</td>
+   		<td class="col-xs-4">390&euro;</td>
+   </tr>	
+</table>
+
+<!-- Tableau pour le week-end -->
+<h2>Tarification pour le week-end</h2>
+<table class="row">
+
+   <tr>
+       <th class="col-xs-8">Saison</th>
+       <th class="col-xs-4">Tarif</th>
+   </tr>
+   <tr>
+   		<td class="col-xs-8">Haute Saison</td>
+   		<td class="col-xs-4">400&euro;</td>
+   </tr>
+   <tr>
+   		<td class="col-xs-8">Autres</td>
+   		<td class="col-xs-4">220&euro;</td>
+   </tr>	
+</table>
+
+<p>
+Informations compl&eacute;mentaires : 
+	<ul>
+		<li>Montant de la caution : 300 €</li>
+		<li>Montant de la caution animal : 100.00 €</li>
+		<li>Electricité : 8 kwh/jour inclus. Consommation suppl&eacute;mentaire facturée selon compteur.</li>
+	</ul>
+</p>
