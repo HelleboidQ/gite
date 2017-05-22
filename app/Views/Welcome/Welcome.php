6e598865663c8a32eb8e3101702a1bf39f9c9538
Dylan Lecigne <dylanlecigne@MacBook-Air-de-Dylan.local>
2017-05-22 23:01:26 +0200

    Modif page d'accueil

--- app/Views/Welcome/Welcome.php
+++ app/Views/Welcome/Welcome.php
@@ -1,27 +1,13 @@
 <div class="container">
 
+    
+
     <div class="row">
         <div class="box">
-           <!-- <div class="col-lg-12 text-center">
-
-                <h2 class="brand-before">
-                    <small>Welcome to</small>
-                </h2>
-                <h1 class="brand-name">Business Casual</h1>
-                <hr class="tagline-divider">
-                <h2>
-                    <small>By
-                        <strong>Start Bootstrap</strong>
-                    </small>
-                </h2>
-            </div>-->
+            <div class="col-lg-6">
             <img src="app/Templates/Business/Assets/images/gite/ext.jpg" id="img_accueil" class="img-responsive"/>
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="box">
-            <div class="col-lg-12">
+            </div>
+            <div class="col-lg-6">
                 <hr>
                 <h2 class="intro-text text-center">Venez vous d&eacute;tendre</h2>
                 <hr>
@@ -30,7 +16,7 @@
                     Ce g&icirc;te situ&eacute; dans un petit village paisible pr&egrave;s de St-Omer est l'endroit id&eacute;al pour prendre du bon temps et se reposer.
                 </p>
                 <p>
-                    
+
                 </p>
             </div>
         </div>
