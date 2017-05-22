cdb93a817c81f371bccabb22714b127125dcb1d2
Dylan Lecigne <dylanlecigne@MacBook-Air-de-Dylan.local>
2017-05-22 23:03:04 +0200

    Modificaiton des col sur la page d'accueil

--- app/Views/Welcome/Welcome.php
+++ app/Views/Welcome/Welcome.php
@@ -3,21 +3,23 @@
     
 
     <div class="row">
-        <div class="box">
-            <div class="col-lg-6">
-            <img src="app/Templates/Business/Assets/images/gite/ext.jpg" id="img_accueil" class="img-responsive"/>
-            </div>
-            <div class="col-lg-6">
-                <hr>
-                <h2 class="intro-text text-center">Venez vous d&eacute;tendre</h2>
-                <hr>
-                <hr class="visible-xs">
-                <p>
-                    Ce g&icirc;te situ&eacute; dans un petit village paisible pr&egrave;s de St-Omer est l'endroit id&eacute;al pour prendre du bon temps et se reposer.
-                </p>
-                <p>
+        <div class="box col-xs-12">
+            <div class="row">
+                <div class="col-md-6 col-xs-12">
+                    <img src="app/Templates/Business/Assets/images/gite/ext.jpg" id="img_accueil" class="img-responsive"/>
+                </div>
+                <div class="col-md-6 col-xs-12">
+                    <hr>
+                    <h2 class="intro-text text-center">Venez vous d&eacute;tendre</h2>
+                    <hr>
+                    <hr class="visible-xs">
+                    <p>
+                        Ce g&icirc;te situ&eacute; dans un petit village paisible pr&egrave;s de St-Omer est l'endroit id&eacute;al pour prendre du bon temps et se reposer.
+                    </p>
+                    <p>
 
-                </p>
+                    </p>
+                </div>
             </div>
         </div>
     </div>
