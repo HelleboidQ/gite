1e7272bf5421d15e24b6267819ca9d7f8995a849
Quentin Helleboid <Quentin@MacBook-Air-de-Quentin.local>
2017-05-22 22:15:39 +0200

    Modif mention legal

---
+++ app/Views/MentionsLegales/mentionsLegales.php
@@ -0,0 +1,22 @@
+<style>
+#ml{
+	color:white;
+}
+</style>
+
+
+<div id="ml">
+<h1>Mentions légales</h1>
+Raison sociale : Gîte de Langlebert</br>
+1 route de serques - 62910 - Moulle </br>
+Directeur de la publication : Monsieur Helleboid Quentin.</br></br>
+Hébergeur</br>
+OVH, 2 rue Kellermann - 59100 Roubaix, France.</br>
+
+<h2>Cookies</h2>
+Lorsque vous naviguez sur le site du gîte de Langlebert, vous autorisez par défaut à stocker des cookies dans votre navigateur. Si vous ne souhaitez pas que ce site stocke des cookies, vous pouvez les désactiver en modifiant les paramètres de votre navigateur. 
+L'utilisation des cookies sur ce site, viennent principalement de Google Analytics, un service d’analyse de sites internet qui permet d'améliorer la qualité du site internet en conaisant des informations relatives sur l'utilisation de celui-ci.
+
+</br></br></br></br>
+
+</div>
