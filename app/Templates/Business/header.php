9d099e92e6bebee792d7c99e9373304c6b072c24
Quentin Helleboid <Quentin@MacBook-Air-de-Quentin.local>
2017-05-23 09:27:49 +0200

    Ajout wiewport

--- app/Templates/Business/header.php
+++ app/Templates/Business/header.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <title><?php echo $title . ' - ' . SITETITLE; ?></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0">
         <?php
         echo $meta; //place to pass data / plugable hook zone
         Assets::css([
