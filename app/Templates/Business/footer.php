4dd147751c39946305a078b188801f67cb8c3a46
Quentin Helleboid <Quentin@MacBook-Air-de-Quentin.local>
2017-05-27 20:41:58 +0200

    Modif lien inutil

--- app/Templates/Business/footer.php
+++ app/Templates/Business/footer.php
@@ -3,9 +3,7 @@
 <?php
 Assets::js([
 	'https://code.jquery.com/jquery-1.12.1.min.js',
-    'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js',
-     Url::templatePath().'js/bootstrap.min.css',
-     Url::templatePath().'js/jquery.css',
+    'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js'
 ]);
 echo $js; //place to pass data / plugable hook zone
 echo $footer; //place to pass data / plugable hook zone
